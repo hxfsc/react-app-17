470b80eeccdc7d52e2d04ada653845d9b011118e
胡孝飞 <hxfsc2@gmail.com>
2021-08-25 19:58:53 +0800

    feat: dashboard page

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,65 +1,52 @@
-import React from "react"
+import React, { useState } from "react"
 
-import { Layout, Menu, Breadcrumb } from "antd"
-import { UserOutlined, LaptopOutlined, NotificationOutlined } from "@ant-design/icons"
+import { Layout, Menu } from "antd"
+import { MenuUnfoldOutlined, MenuFoldOutlined, UserOutlined, VideoCameraOutlined, UploadOutlined } from "@ant-design/icons"
 
-const { SubMenu } = Menu
-const { Header, Content, Sider } = Layout
-
+const { Header, Sider, Content } = Layout
 
 import styles from "./index.scss"
 
 export const Dashboard = () => {
+  const [collapsed, setCollapsed] = useState<boolean>(false)
+
+  const dashboardTrigger = (collapsed: boolean): void => {
+    setCollapsed(collapsed)
+  }
+
   return (
     <Layout className={styles.dashboard}>
-      <Header className="header">
-        <div className="logo" />
-        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["2"]}>
-          <Menu.Item key="1">nav 1</Menu.Item>
-          <Menu.Item key="2">nav 2</Menu.Item>
-          <Menu.Item key="3">nav 3</Menu.Item>
+      <Sider trigger={null} collapsible collapsed={collapsed}>
+        <div className={styles.logo} />
+        <Menu theme="dark" mode="inline" defaultSelectedKeys={["1"]}>
+          <Menu.Item key="1" icon={<UserOutlined />}>
+            nav 1
+          </Menu.Item>
+          <Menu.Item key="2" icon={<VideoCameraOutlined />}>
+            nav 2
+          </Menu.Item>
+          <Menu.Item key="3" icon={<UploadOutlined />}>
+            nav 3
+          </Menu.Item>
         </Menu>
-      </Header>
-      <Layout>
-        <Sider width={200} className="site-layout-background">
-          <Menu mode="inline" defaultSelectedKeys={["1"]} defaultOpenKeys={["sub1"]} style={{ height: "100%", borderRight: 0 }}>
-            <SubMenu key="sub1" icon={<UserOutlined />} title="subnav 1">
-              <Menu.Item key="1">option1</Menu.Item>
-              <Menu.Item key="2">option2</Menu.Item>
-              <Menu.Item key="3">option3</Menu.Item>
-              <Menu.Item key="4">option4</Menu.Item>
-            </SubMenu>
-            <SubMenu key="sub2" icon={<LaptopOutlined />} title="subnav 2">
-              <Menu.Item key="5">option5</Menu.Item>
-              <Menu.Item key="6">option6</Menu.Item>
-              <Menu.Item key="7">option7</Menu.Item>
-              <Menu.Item key="8">option8</Menu.Item>
-            </SubMenu>
-            <SubMenu key="sub3" icon={<NotificationOutlined />} title="subnav 3">
-              <Menu.Item key="9">option9</Menu.Item>
-              <Menu.Item key="10">option10</Menu.Item>
-              <Menu.Item key="11">option11</Menu.Item>
-              <Menu.Item key="12">option12</Menu.Item>
-            </SubMenu>
-          </Menu>
-        </Sider>
-        <Layout style={{ padding: "0 24px 24px" }}>
-          <Breadcrumb style={{ margin: "16px 0" }}>
-            <Breadcrumb.Item>Home</Breadcrumb.Item>
-            <Breadcrumb.Item>List</Breadcrumb.Item>
-            <Breadcrumb.Item>App</Breadcrumb.Item>
-          </Breadcrumb>
-          <Content
-            className="site-layout-background"
-            style={{
-              padding: 24,
-              margin: 0,
-              minHeight: 280
-            }}
-          >
-            Content
-          </Content>
-        </Layout>
+      </Sider>
+      <Layout className="site-layout">
+        <Header className={styles["site-layout-background"]} style={{ padding: 0 }}>
+          {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
+            className: "trigger",
+            onClick: () => dashboardTrigger(!collapsed)
+          })}
+        </Header>
+        <Content
+          className={styles["site-layout-background"]}
+          style={{
+            margin: "24px 16px",
+            padding: 24,
+            minHeight: 280
+          }}
+        >
+          Content
+        </Content>
       </Layout>
     </Layout>
   )
